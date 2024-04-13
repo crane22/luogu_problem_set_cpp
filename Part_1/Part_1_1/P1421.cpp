abb6f13edc5808a2564cd551bff0c7dd95ee2ada
crane <i@crane.moe>
2024-04-13 20:09:24 +0800

    First commit.

---
+++ Part_1/Part_1_1/P1421.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << (a * 10 + b) / (1 * 10 + 9);
+}
